3b3afa3631b84455f00adec542c8354417cd8d93
ujjwal sharma <ujjwalsharma@ujjwals-MacBook-Pro.local>
2024-09-16 02:54:57 +0530

    jsonify the data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import os
 # from dotenv import load_dotenv
+import json
 from openai import APIConnectionError
 import openai
 import streamlit as st
@@ -7,7 +8,18 @@ import streamlit as st
 # txt = st.text_input("Enter prompt")
 # st.write(f"You: {txt}")
 
-prompt = """You are a book reviewer that tells the genre of a book summary with how much percent of a genre is involved and give a list of similar books. give the percentages in tabular form. Do not give extra iinformation like summary. The genres are """
+prompt = """You are a book reviewer that tells the genre of a book summary with how much percent of a genre is involved and give a list of similar books. Do not give extra information like summary. The genres are 
+return the response in this exact json format:
+{
+    "BookName": "Name Of Book",
+    "AuthorName": "Name Of Author",
+    "GenreClassification": {
+        "Short Story": "70",
+        "Memoir & Autobiography": "30"
+    },
+    "SimilarBooks": ["Book1", "Book2"]
+}
+"""
 promptHelperText = "onlyGenres.txt"
 
 def getGenreInfo():
@@ -38,6 +50,18 @@ def mainn():
         )
         # print(completion.choices[0].message.content)
         st.write(completion.choices[0].message.content)
+
+        data = json.loads(completion.choices[0].message.content)
+
+        try:
+            with open("data.json", "r") as file:
+                existing_data = json.load(file)
+        except FileNotFoundError:
+            existing_data = []
+        existing_data.append(data)
+
+        with open("data.json", "w") as file:
+            json.dump(existing_data, file, indent=4)
 
         break
 
